cd736a769649d52b70a05ef19fed38175d74ddbf
Andrew Weddle <aweddle@gmail.com>
2023-06-03 22:35:02 +1000

    places api working with config file

--- LocationParser/PlacesApi.py
+++ LocationParser/PlacesApi.py
@@ -1,17 +1,50 @@
 # May replace this with the entire Python Library, but for now it's just one API call
 import requests
+import json
 from LocationParser.Config import Config
 
 def ResolvePlaceName(text):
     waffle = Config()
     apiKey = waffle.Config['GoogleApi']['PlacesApiKey']
 
-    url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input="+text+"%20Australia&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key="+apiKey
+    url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input="+text+"%20Australia&inputtype=textquery&fields=formatted_address%2Cname%2Cgeometry&key="+apiKey
 
     payload={}
     headers = {}
 
     response = requests.request("GET", url, headers=headers, data=payload)
 
-    print(response.text)
+    return GooglePlacesResponse.from_json(json.loads(response.text))
 
+class GooglePlacesResponse:
+    def __init__(self, formatted_address, lat, lng, name):
+        self.formatted_address = formatted_address
+        self.lat = lat
+        self.lng = lng
+        self.name = name
+
+    def __iter__(self):
+        yield from {
+            "formatted_address": self.formatted_address,
+            "lat": self.lat,
+            "lng": self.lng,
+            "name": self.name
+        }.items()
+
+    def __str__(self):
+        return json.dumps(dict(self), ensure_ascii=False)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def to_json(self):
+        return self.__str__()
+    
+    @staticmethod
+    def from_json(json_dct):
+      # TODO error checking needed here.
+      return GooglePlacesResponse(json_dct['candidates'][0]['formatted_address'],
+                   json_dct['candidates'][0]['geometry']['location']['lat'],
+                   json_dct['candidates'][0]['geometry']['location']['lng'],
+                   json_dct['candidates'][0]['name']
+            )
--- tests/PlacesApiTests.py
+++ tests/PlacesApiTests.py
@@ -5,10 +5,16 @@ class PleacesApiTests(unittest.TestCase):
 
     def test1(self):
         location1 = ResolvePlaceName("the London Hotel Ardlethen NSW")
-        #self.assertEqual("xxxx", location1.text)
+        self.assertEqual("London Hotel-Motel", location1.name)
+        self.assertEqual(-34.3577734, location1.lat)
+        self.assertEqual(146.897691, location1.lng)
+        self.assertEqual("12 Mirrool St, Ardlethan NSW 2665, Australia", location1.formatted_address)
 
         location2 = ResolvePlaceName("Rudds Pub")
-        #self.assertEqual("xxxx", location2.text)
+        self.assertEqual("Rudd's Pub", location2.name)
+        self.assertEqual(-27.8525601, location2.lat)
+        self.assertEqual(151.9032221, location2.lng)
+        self.assertEqual("45 Tooth St, Nobby QLD 4360, Australia", location2.formatted_address)
 
 if __name__ == '__main__':
     unittest.main()
