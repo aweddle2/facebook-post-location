e991e2938070703e440ead63d7e5364112061eea
Andrew Weddle <aweddle@gmail.com>
2023-06-11 17:00:30 +1000

    refactored and tidied up

--- LocationParser/GoogleSheetApi.py
+++ LocationParser/GoogleSheetApi.py
@@ -8,46 +8,17 @@ from google.oauth2 import service_account
 
 
 def Append(values: list):
-    payload = GoogleSheetsAppendPayload(values).to_json()
-    # Define the auth scopes to request.
-    scope = 'https://www.googleapis.com/auth/spreadsheets'
-    key_file_location = '/Users/asw/Downloads/facebook-post-location-9367bc1f3fe0.json'
-
-    # Authenticate and construct service.
-    service = get_service(
-        api_name='sheets',
-        api_version='v4',
-        scopes=[scope],
-        key_file_location=key_file_location)
-
-    update_data(service, payload)
-
-
-def get_service(api_name, api_version, scopes, key_file_location):
-
-    credentials = service_account.Credentials.from_service_account_file(
-        key_file_location)
-
-    # Build the service object.
-    service = build(api_name, api_version, credentials=credentials)
-
-    return service
-
-
-def update_data(service, body):
-    body = "{\"values\":[[\"text\",\"from\",\"a\",\"unit\",\"test\"]]}"
 
     conf = Config()
+    serviceAccountKeyFileLocation = conf.Config['GoogleApi']['ServiceAccountKeyFileLocation']
     spreadsheetId = conf.Config['GoogleApi']['SpreadsheetId']
 
-    return service.spreadsheets().values().append(
+    # Authenticate and construct service.
+    credentials = service_account.Credentials.from_service_account_file(
+        serviceAccountKeyFileLocation)
+
+    # Build the service object.
+    service = build('sheets', 'v4', credentials=credentials)
+    service.spreadsheets().values().append(
         spreadsheetId=spreadsheetId, range="A:Z",
-        valueInputOption="USER_ENTERED", body=json.loads(body)).execute()
-
-
-class GoogleSheetsAppendPayload:
-    def __init__(self, values):
-        self.values = values
-
-    def to_json(self):
-        return self.__str__()
+        valueInputOption="USER_ENTERED", body={'values': [values]}).execute()
--- tests/GoogleSheetApiTests.py
+++ tests/GoogleSheetApiTests.py
@@ -5,7 +5,7 @@ from LocationParser.GoogleSheetApi import Append
 class PlacesApiTests(unittest.TestCase):
 
     def test1(self):
-        response = Append({"text", "from", "a", "unit", "test"})
+        Append(["text", "from", "a", "unit", "test"])
 
 
 if __name__ == '__main__':
