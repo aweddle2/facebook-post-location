c416638a60eef0af14852b63cd382c9147e0bfbd
Andrew Weddle <aweddle@gmail.com>
2023-06-15 23:05:33 +1000

    don't add duplicates based on Facebook Permalink

--- FacebookPostLocation/GoogleSheetApi.py
+++ FacebookPostLocation/GoogleSheetApi.py
@@ -1,10 +1,9 @@
-# May replace this with the entire Python Library, but for now it's just one API call
-import requests
-import json
 from FacebookPostLocation.Config import Config
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from google.oauth2 import service_account
+
+hiddenSheetName = "LOOKUP_SHEET"
 
 
 def Append(values: list):
@@ -16,9 +15,38 @@ def Append(values: list):
     # Authenticate and construct service.
     credentials = service_account.Credentials.from_service_account_file(
         serviceAccountKeyFileLocation)
-
-    # Build the service object.
     service = build('sheets', 'v4', credentials=credentials)
-    service.spreadsheets().values().append(
-        spreadsheetId=spreadsheetId, range="A:A",
-        valueInputOption="USER_ENTERED", body={'values': [values]}).execute()
+
+    # See of the search sheet exists and create if not
+    ranges = [hiddenSheetName+"!A1:B1"]
+    getFormulaCells = service.spreadsheets().get(spreadsheetId=spreadsheetId,
+                                                 ranges=ranges, includeGridData=True)
+    try:
+        response = getFormulaCells.execute()
+    except:
+        createSheetPayload = {"requests": [
+            {"addSheet": {"properties": {"hidden": True, "title": hiddenSheetName}}}, ]}
+        response = service.spreadsheets().batchUpdate(
+            spreadsheetId=spreadsheetId, body=createSheetPayload).execute()
+
+    # Set the formula
+    # TODO unhardcode this, probably by passing in an object now to this method rather than just a list
+    url = values[6]
+    formula = '=COUNTIF(Data!G:G, "'+url+'")'
+    addFormulaPayload = {
+        "range": hiddenSheetName+"!A1",
+        "majorDimension": "COLUMNS",
+        "values": [[formula]]
+    }
+
+    response = service.spreadsheets().values().update(
+        spreadsheetId=spreadsheetId, range=hiddenSheetName+"!A1", body=addFormulaPayload, valueInputOption="USER_ENTERED", includeValuesInResponse=True).execute()
+
+    # Check to see if the data is in the data sheet by looking at the response from the formula
+    urlCount = response['updatedData']['values'][0][0]
+
+    if (urlCount == '0'):
+        # Append the data if it does not exist
+        service.spreadsheets().values().append(
+            spreadsheetId=spreadsheetId, range="Data!A:A",
+            valueInputOption="USER_ENTERED", body={'values': [values]}).execute()
--- tests/GoogleSheetApiTests.py
+++ tests/GoogleSheetApiTests.py
@@ -5,7 +5,8 @@ from FacebookPostLocation.GoogleSheetApi import Append
 class PlacesApiTests(unittest.TestCase):
 
     def test1(self):
-        Append(["text", "from", "a", "unit", "test"])
+        Append(["Lorem Ipsum", "Loral Ipsum (Aust) Pty Ltd", "22-26 Jersey Rd, Bayswater VIC 3153, Australia",
+               "-37.8413503", "145.2793097", "", "https://www.facebook.com/groups/1252515138743868/permalink/1252516268743755/"])
 
 
 if __name__ == '__main__':
